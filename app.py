28f84a53dfb010766ee22f944b3dc3d0f3585cee
Cristian <681004@alu.murciaeduca.es>
2026-02-13 12:24:25 +0100

    Añadiendo fichero frases y nuevas funciones bayeta.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from bayeta import frotar
 
 # Imprime en la terminal el texto 'Hola mundo'
-print("Hola mundo")
+#print("Hola mundo")
+frotar()
---
+++ bayeta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import random
+
+def cargar_frases(ruta):
+    with open(ruta, "r", encoding="utf-8") as archivo:
+        frases_leidas = archivo.readlines()
+
+    # Quitar saltos de línea
+    frases_leidas = [frases_leidas.strip() for frases_leidas in frases_leidas]
+
+    return frases_leidas
+# Lista de frases auspiciosas
+FRASES = cargar_frases("/home/cristian/Documents/Proyectos_Git/balleta_fortuna/pps_python_git_docker/frases.txt")
+
+#Función frotar
+def frotar(n_frases: int = 1) -> list:
+    pass
+    # Seleccionamos aleatoriamente las frases
+    return print(random.choices(FRASES, k=n_frases))
+
